449d0b37041f052870f9be8d28eabb5a41a9984a
derekbt96 <derekbt@yahoo.com>
2017-10-26 14:31:08 -0400

    fix

--- Mass_DBF.m
+++ Mass_DBF.m
@@ -6,17 +6,17 @@ Mass.MTOM = MTOM;   %kg
 % V_cruise_ms             = VbestLD_ms;
 % q_cruise                = .5*rho*(V_cruise_ms).^2; %kg/(m.s^2) or Pa
 
+
+% Mass.EW_fract      = .65; %assumption
+% Mass.EW_fract      = .70; %assumption
+
 MassFrac0 = 1.5/MTOM;
-Mass.EW_fract      = .65; %assumption
-
-
-Mass.EW_fract      = .70; %assumption
 Mass.payload       = 10*0.0283495;      %oz to kg
 
 Mass.EW_fract      = MassFrac0- .1 + .2*(Geometry.W_S/130); %assumption
 Mass.payload_frac      = Mass.payload/MTOM;  %assumption
 
-Mass.remaining_fraction     = 1 - Mass.EW_fract;
+Mass.remaining_fraction     = 1 - Mass.EW_fract - Mass.payload_frac;
 Mass.remaining              = Mass.remaining_fraction*MTOM;
 Mass.Mbatteries             = Mass.remaining - Mass.payload;
 
